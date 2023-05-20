c378287647cb422a6758243eeb8a738fd184281f
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-05-20 16:07:12 +0600

    install every thing

--- readme.md
+++ readme.md
@@ -1,1 +1,14 @@
 1. npm init
+2. npm install typescript --save-dev 
+3. npm install express --save
+4. npm install mongoose --save
+5. npm i ts-node-dev --save-dev
+
+
+
+start using npm strat
+
+
+6. npm i cors
+7. npm i --save-dev @types/cors
+8. npm i --save-dev @types/express
---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose"
+import app from "./app";
+
+
+const port: number = 5000
+
+
+
+async function bootstrap() {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/test');
+        console.log('first ggg')
+        app.listen(port, () => {
+            console.log(`Server Running on Port ${port}`)
+        })
+    } catch (err) {
+        console.log('faild to connect databae', err)
+    }
+}
+
+
+bootstrap()
+
+
+
+
+

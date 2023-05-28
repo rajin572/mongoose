f02ad86e0a8cd212f328ec13cb4888ff1064077b
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-05-28 19:21:53 +0600

    t

--- src/app/module/user/user.services.ts
+++ src/app/module/user/user.services.ts
@@ -7,7 +7,6 @@ export const getUserFromDB = async ():Promise<IUser[]> => {
     return users
 }
 
-
 export const getUserByIdFromDB = async (payload:string): Promise<IUser | null> =>{
     const user = await User.findOne({id:payload})    
     return user
